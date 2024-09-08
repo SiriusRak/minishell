81cc9c4781f3af0990e1ffc79add4937d3073d26
SiriusRak <diaryrakitrniela@gmail.com>
2024-09-08 16:12:09 +0300

    first commit

---
+++ includes/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdiary <rdiary@student.42antananarivo      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 16:06:04 by rdiary            #+#    #+#             */
+/*   Updated: 2024/09/08 16:11:17 by rdiary           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+void	get_input(void);
+
+#endif
